a72ed4301555aee197bd3930d3b59f37f4cb1595
Thomas.G <thomas.girotti@cy-tech.fr>
2024-03-30 15:56:44 +0100

    Connexion update

--- PHP/Client/Connexion.php
+++ PHP/Client/Connexion.php
@@ -5,14 +5,7 @@ if (isset($_SESSION['loggedin'])) {
     exit;
 } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['username']) && isset($_POST['password'])) {
-        $DATABASE_HOST = '127.0.0.1';
-        $DATABASE_USER = 'testid';
-        $DATABASE_PASS = 'testmdp';
-        $DATABASE_NAME = 'personalytree';
-        $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-        //if ( mysqli_connect_errno() ) {
-        //    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-        //}
+        include 'BackEnd/LoginDatabase.php';
         if ($stmt = $con->prepare('SELECT id, password FROM login WHERE username = ?')) {
             $stmt->bind_param('s', $_POST['username']);
             $stmt->execute();
