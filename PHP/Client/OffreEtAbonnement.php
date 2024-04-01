bd71b69025902748c2bdbc6a55eb998a5b2eb254
Smooss <132471515+Smooss@users.noreply.github.com>
2024-04-01 22:56:07 +0200

    Update OffreEtAbonnement.php

---
+++ PHP/Client/OffreEtAbonnement.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="OffreEtAbonnement.css">
+    <title>PersonaliTree - Rencontre par affinité naturelle</title>
+</head>
+
+<body>
+    
+    <div class="container">
+        <div class="box1">
+            <div class="int">
+                <div class="int_h2">
+                    <h2>Découvrez notre offre d'abonnement</h2>
+                </div>
+                <div class="separator"></div>
+                <div class="int_h1">
+                    <h1>Pour profiter de toutes les fonctionnalités de PersonaliTree n'attendez plus et souscrivez tout de suite à un abonnement</h1>
+                </div>
+                <div class="separator"></div>
+                <div class="int_p">
+                    <p>+ Consulter le profil des autres utilisateurs </p>
+                    <p>+ Connaitre les utilisateurs qui visite votre profil</p>
+                    <p>+ Echanger des messages avec les autres utilisateurs</p>
+                    <p>+ Bloquer des utilisateurs</p>
+                </div>
+                <div class="int_a">
+                    <a class="button" href="">S'abonner</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
