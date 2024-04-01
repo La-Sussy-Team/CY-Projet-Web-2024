920a1624090d5d18348b1d357802b0888eceb184
Lilian <106081036+Sulfy6768@users.noreply.github.com>
2024-04-01 11:39:41 +0200

    Create erreur.php
    pour tout type d'erreur

---
+++ PHP/Client/erreur.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Erreur</title>
+</head>
+<body>
+    <h1>Une erreur s'est produite</h1>
+    <p>Désolé, une erreur s'est produite. Veuillez réessayer ultérieurement.</p>
+</body>
+</html>
