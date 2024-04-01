2ec23198e82e41f4fe812f6d2c649351006d1e90
Lilian <106081036+Sulfy6768@users.noreply.github.com>
2024-04-01 23:00:53 +0200

    Update OffreEtAbonnement.php

--- PHP/Client/OffreEtAbonnement.php
+++ PHP/Client/OffreEtAbonnement.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="OffreEtAbonnement.css">
+    <link rel="stylesheet" href="../CSS/OffreEtAbonnement.css">
     <title>PersonaliTree - Rencontre par affinité naturelle</title>
 </head>
 
