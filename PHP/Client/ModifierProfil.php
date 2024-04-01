c39f398d39bc1a13c2d5fa0e971e0a0038833228
Sulfy6768 <liliandel.etoile@gmail.com>
2024-04-01 23:22:18 +0200

    modifcss

--- PHP/Client/ModifierProfil.php
+++ PHP/Client/ModifierProfil.php
@@ -22,6 +22,7 @@ if ($stmt = $con->prepare('SELECT * FROM infopersos WHERE user_id = ?')) {
     <link rel="icon" href="../../Assets/Logo/Logo_Fullscreen.png" type="img/png">
     <title>PersonaliTree - Modification Profil</title>
     <link rel="stylesheet" href="../../CSS/Client/ModifierProfil.css">
+    <link rel="stylesheet" href="../../CSS/Client/Inscription.css">
     <script src="../../JS/Librairies/jquery-3.7.1.min.js"></script>
     <script src="../../JS/Client/Modification.js"></script>
 </head>
@@ -29,21 +30,25 @@ if ($stmt = $con->prepare('SELECT * FROM infopersos WHERE user_id = ?')) {
 include "Header.php";
 ?>
 <body>
-    <img id="profilePic" src="../../Assets/Client/ProfileImage/<?php echo $user['imgpath']?>" onclick="document.getElementById('profileImage').click();" style="cursor: pointer;">
-    <input type="hidden" id="currentProfileImage" name="currentProfileImage" value="<?php echo $user['imgpath']; ?>">
-    <input type="file" id="profileImage" name="profileImage" accept="image/*" style="display: none;" onchange="loadFile(event)">
-    <label for="currentPassword">Current Password:</label> <input type="password" id="currentPassword" name="currentPassword" required><br>
-    <label for="username">Username:</label> <input type="text" id="username" name="username" value="<?php echo $login['username']; ?>" readonly><br>
-    <label for="email">Email:</label> <input type="email" id="email" name="email" value="<?php echo $user['email']; ?>"><br>
-    <label for="tel">Tel:</label> <input type="tel" id="tel" name="tel" value="<?php echo $user['phone']; ?>"><br>
-    <label for="password">New Password:</label> <input type="password" id="password" name="password"><br>
-    <label for="passwordConfirmation">Confirm New Password:</label> <input type="password" id="passwordConfirmation" name="passwordConfirmation"><br>
-    <label for="prenom">Prenom:</label> <input type="text" id="prenom" name="prenom" value="<?php echo $user['prenom']; ?>"><br>
-    <label for="nom">Nom:</label> <input type="text" id="nom" name="nom" value="<?php echo $user['nom']; ?>"><br>
-    <label for="sexe">Sexe:</label> <input type="text" id="sexe" name="sexe" value="<?php echo $user['sexe']; ?>"><br>
-    <label for="pays">Pays:</label> <input type="text" id="pays" name="pays" value="<?php echo $user['pays']; ?>"><br>
-    <label for="ville">Ville:</label> <input type="text" id="ville" name="ville" value="<?php echo $user['ville']; ?>"><br>
-    <label for="adresse">Adresse:</label> <input type="text" id="adresse" name="adresse" value="<?php echo $user['adresse']; ?>"><br>
-    <button class="send" id="send">Envoyer les modifications</button>
+    <div class="register-container">
+        <div class="modifie">
+            <img id="profilePic" src="../../Assets/Client/ProfileImage/<?php echo $user['imgpath']?>" onclick="document.getElementById('profileImage').click();" style="cursor: pointer;">
+            <input type="hidden" id="currentProfileImage" name="currentProfileImage" value="<?php echo $user['imgpath']; ?>">
+            <input type="file" id="profileImage" name="profileImage" accept="image/*" style="display: none;" onchange="loadFile(event)">
+            <label for="currentPassword">Current Password:</label> <input type="password" id="currentPassword" name="currentPassword" required><br>
+            <label for="username">Username:</label> <input type="text" id="username" name="username" value="<?php echo $login['username']; ?>" readonly><br>
+            <label for="email">Email:</label> <input type="email" id="email" name="email" value="<?php echo $user['email']; ?>"><br>
+            <label for="tel">Tel:</label> <input type="tel" id="tel" name="tel" value="<?php echo $user['phone']; ?>"><br>
+            <label for="password">New Password:</label> <input type="password" id="password" name="password"><br>
+            <label for="passwordConfirmation">Confirm New Password:</label> <input type="password" id="passwordConfirmation" name="passwordConfirmation"><br>
+            <label for="prenom">Prenom:</label> <input type="text" id="prenom" name="prenom" value="<?php echo $user['prenom']; ?>"><br>
+            <label for="nom">Nom:</label> <input type="text" id="nom" name="nom" value="<?php echo $user['nom']; ?>"><br>
+            <label for="sexe">Sexe:</label> <input type="text" id="sexe" name="sexe" value="<?php echo $user['sexe']; ?>"><br>
+            <label for="pays">Pays:</label> <input type="text" id="pays" name="pays" value="<?php echo $user['pays']; ?>"><br>
+            <label for="ville">Ville:</label> <input type="text" id="ville" name="ville" value="<?php echo $user['ville']; ?>"><br>
+            <label for="adresse">Adresse:</label> <input type="text" id="adresse" name="adresse" value="<?php echo $user['adresse']; ?>"><br>
+            <button class="send" id="send">Envoyer les modifications</button>
+        </div>
+    </div>
 </body>
 </html>
