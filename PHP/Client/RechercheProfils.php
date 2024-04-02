92a9208ab508ddd0f4074c951a2be103679855b3
Sou <pierreantoine.cassard@gmail.com>
2024-04-02 17:19:00 +0200

    recherche profil CSS pt1

--- PHP/Client/RechercheProfils.php
+++ PHP/Client/RechercheProfils.php
@@ -44,14 +44,15 @@ session_start();
     </style>
 </head>
 <link rel="stylesheet" type="text/css" href="../../CSS/Client/Menu_Site.css">
+<link rel="stylesheet" type="text/css" href="../../CSS/Client/RechercheProfils.css">
 <div class="bar">
     <div class="bar_1">
         <img class="logo1" src="../../Assets/Logo/Logo_Fullscreen.png">
     </div>
     <div class="bar_2">
         <ul class="navbar">
-            <li><a href="./Accueil.php" style="color: white; text-decoration: none; display: flex; align-items: center;">Accueil</a></li>
-            <li><a href="./Client/RechercheProfils.php" style="color: white; text-decoration: none; display: flex; align-items: center;">Recherche de profils</a></li>
+            <li><a href="../Accueil.php" style="color: white; text-decoration: none; display: flex; align-items: center;">Accueil</a></li>
+            <li><a href="./RechercheProfils.php" style="color: white; text-decoration: none; display: flex; align-items: center;">Recherche de profils</a></li>
             <li><a href="../../PHP/Client-Pages/Carte.php" style="color: white; text-decoration: none; display: flex; align-items: center;">Carte des contacts</a></li>
         </ul>
     </div>
@@ -72,15 +73,42 @@ session_start();
         <h1>PersonaliTree</h1>
         <p>Rencontrez des personnes partageant votre affinité naturelle à travers une plante ou un arbre.</p>
     </header>
-    <div class="search-bar">
+    <div class="filter-bar" id="filter-bar">
+        <div class="deroulant">
         <form method="post">
-            <input type="text" placeholder="Nom" name="search-nom">
-            <input type="text" placeholder="Ville" name="search-ville">
+            <table>
+                <tr>
+                    <th><input type="text" class="filter-searchbar" placeholder="Nom" name="search-nom"><br></th>
+                    <th><input type="checkbox" class="checkbox-sex" name="sex1"> <label for="sex1">Homme</label></th>
+                </tr>
+                <tr>
+                    <th><input type="text" class="filter-searchbar" placeholder="Ville" name="search-ville"><br></th>
+                    <th><input type="checkbox" class="checkbox-sex" name="sex2"> <label for="sex2">Femme</label></th>
+                </tr>
+                <tr>
+                    <th><input type="text" class="filter-searchbar" placeholder="Pays" name="search-pays"></th>
+                    <th><input type="checkbox" class="checkbox-sex" name="sex3"> <label for="sex3">Autre</label></th>
+                </tr>
+            </table>
             <button name="submit">Rechercher</button>
         </form>
     </div>
+    </div>
+    <div class="unroll-button" onclick="menu()">
+        <script>
+            function menu(){
+            const menu = document.getElementById("filter-bar");
+            if(menu == null){
+                console.log("oops");
+            }
+            menu.classList.add("active");
+            }
+        </script>
+        <p>+</p>
+    </div>
     <div class="result">
         <table class="table">
+
             <?php
             if(isset( $POST['submit'])) {
                 $nom=$_POST['search-nom'];
@@ -89,7 +117,7 @@ session_start();
                 $sql="Select * from infopersos where nom like '%$nom%'";
                 $result=mysqli_query($conn,$sql);
                 if($result){
-                    echo"console.log('noice')";
+                    echo"<p>wow</p>";
                 }
             }
             ?>
