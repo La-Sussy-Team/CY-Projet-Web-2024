0985802e730497d542044a56ebdd32f6551c34dd
Smooss <132471515+Smooss@users.noreply.github.com>
2024-04-02 16:34:16 +0200

    Update OffreEtAbonnement.php

--- PHP/Client/OffreEtAbonnement.php
+++ PHP/Client/OffreEtAbonnement.php
@@ -6,12 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../CSS/Client/OffreEtAbonnement.css">
     <title>PersonaliTree - Rencontre par affinité naturelle</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
         function confirmerAbonnement() {
             if (confirm("Êtes-vous sûr de vouloir vous abonner ?")) {
-                alert("Vous êtes maintenant abonné !");
-            } else {
-                alert("Vous n'êtes pas abonné !");
+                // Envoi d'une requête AJAX pour mettre à jour le statut d'abonnement
+                $.ajax({
+                    url: 'MajStatutAbo.php',
+                    type: 'POST',
+                    data: { abonnement: 'abonne' },
+                    success: function(response) {
+                        // Si la mise à jour réussit, afficher un message de confirmation
+                        alert("Vous êtes maintenant abonné !");
+                    },
+                    error: function(xhr, status, error) {
+                        // En cas d'erreur, afficher un message d'erreur
+                        alert("Une erreur s'est produite lors de la mise à jour de votre statut d'abonnement.");
+                    }
+                });
             }
         }
     </script>
@@ -37,6 +49,7 @@
                     <p>+ Bloquer des utilisateurs</p>
                 </div>
                 <div class="int_a">
+                    <!-- Appel de la fonction confirmerAbonnement() lors du clic sur le lien -->
                     <a class="button" href="#" onclick="confirmerAbonnement()">S'abonner</a>
                 </div>
             </div>
@@ -46,4 +59,3 @@
 </body>
 
 </html>
-
