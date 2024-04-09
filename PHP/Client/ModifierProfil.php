1688aa9fce1c68899997cb191b36b861c7f453ab
ThomasGirotti <girottitho@cy-tech.fr>
2024-04-09 07:27:47 +0200

    Maj CSS

--- PHP/Client/ModifierProfil.php
+++ PHP/Client/ModifierProfil.php
@@ -35,18 +35,18 @@ include "Header.php";
             <img id="profilePic" src="../../Assets/Client/ProfileImage/<?php echo $user['imgpath']?>" onclick="document.getElementById('profileImage').click();" style="cursor: pointer;">
             <input type="hidden" id="currentProfileImage" name="currentProfileImage" value="<?php echo $user['imgpath']; ?>">
             <input type="file" id="profileImage" name="profileImage" accept="image/*" style="display: none;" onchange="loadFile(event)">
-            <label for="currentPassword">Current Password:</label> <input type="password" id="currentPassword" name="currentPassword" required><br>
-            <label for="username">Username:</label> <input type="text" id="username" name="username" value="<?php echo $login['username']; ?>" readonly><br>
-            <label for="email">Email:</label> <input type="email" id="email" name="email" value="<?php echo $user['email']; ?>"><br>
-            <label for="tel">Tel:</label> <input type="tel" id="tel" name="tel" value="<?php echo $user['phone']; ?>"><br>
-            <label for="password">New Password:</label> <input type="password" id="password" name="password"><br>
-            <label for="passwordConfirmation">Confirm New Password:</label> <input type="password" id="passwordConfirmation" name="passwordConfirmation"><br>
-            <label for="prenom">Prenom:</label> <input type="text" id="prenom" name="prenom" value="<?php echo $user['prenom']; ?>"><br>
-            <label for="nom">Nom:</label> <input type="text" id="nom" name="nom" value="<?php echo $user['nom']; ?>"><br>
-            <label for="sexe">Sexe:</label> <input type="text" id="sexe" name="sexe" value="<?php echo $user['sexe']; ?>"><br>
-            <label for="pays">Pays:</label> <input type="text" id="pays" name="pays" value="<?php echo $user['pays']; ?>"><br>
-            <label for="ville">Ville:</label> <input type="text" id="ville" name="ville" value="<?php echo $user['ville']; ?>"><br>
-            <label for="adresse">Adresse:</label> <input type="text" id="adresse" name="adresse" value="<?php echo $user['adresse']; ?>"><br>
+            <label for="currentPassword">Mot de passe actuel :</label> <input type="password" id="currentPassword" name="currentPassword" required><br>
+            <label for="username">Nom d'utilisateur :</label> <input type="text" id="username" name="username" value="<?php echo $login['username']; ?>" readonly><br>
+            <label for="email">Email :</label> <input type="email" id="email" name="email" value="<?php echo $user['email']; ?>"><br>
+            <label for="tel">Telephone :</label> <input type="tel" id="tel" name="tel" value="<?php echo $user['phone']; ?>"><br>
+            <label for="password">Nouveau mot de passe :</label> <input type="password" id="password" name="password"><br>
+            <label for="passwordConfirmation">Confirmer mot de passe :</label> <input type="password" id="passwordConfirmation" name="passwordConfirmation"><br>
+            <label for="prenom">Prenom :</label> <input type="text" id="prenom" name="prenom" value="<?php echo $user['prenom']; ?>"><br>
+            <label for="nom">Nom :</label> <input type="text" id="nom" name="nom" value="<?php echo $user['nom']; ?>"><br>
+            <label for="sexe">Sexe :</label> <input type="text" id="sexe" name="sexe" value="<?php echo $user['sexe']; ?>"><br>
+            <label for="pays">Pays :</label> <input type="text" id="pays" name="pays" value="<?php echo $user['pays']; ?>"><br>
+            <label for="ville">Ville :</label> <input type="text" id="ville" name="ville" value="<?php echo $user['ville']; ?>"><br>
+            <label for="adresse">Adresse :</label> <input type="text" id="adresse" name="adresse" value="<?php echo $user['adresse']; ?>"><br>
             <button class="send" id="send">Envoyer les modifications</button>
         </div>
     </div>
--- PHP/Client/RechercheProfils.php
+++ PHP/Client/RechercheProfils.php
@@ -7,6 +7,8 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../CSS/Client/StylesCommuns.css">
+    <link rel="stylesheet" type="text/css" href="../../CSS/Client/Menu_Site.css">
+    <link rel="stylesheet" type="text/css" href="../../CSS/Client/RechercheProfils.css">
     <title>PersonaliTree - Rencontre par affinité naturelle</title>
     <style>
         body {
@@ -43,31 +45,9 @@ session_start();
         }
     </style>
 </head>
-<link rel="stylesheet" type="text/css" href="../../CSS/Client/Menu_Site.css">
-<link rel="stylesheet" type="text/css" href="../../CSS/Client/RechercheProfils.css">
-<div class="bar">
-    <div class="bar_1">
-        <img class="logo1" src="../../Assets/Logo/Logo_Fullscreen.png">
-    </div>
-    <div class="bar_2">
-        <ul class="navbar">
-            <li><a href="../Accueil.php" style="color: white; text-decoration: none; display: flex; align-items: center;">Accueil</a></li>
-            <li><a href="./RechercheProfils.php" style="color: white; text-decoration: none; display: flex; align-items: center;">Recherche de profils</a></li>
-            <li><a href="../../PHP/Client-Pages/Carte.php" style="color: white; text-decoration: none; display: flex; align-items: center;">Carte des contacts</a></li>
-        </ul>
-    </div>
-    <div class="bar_3">
-        <?php
-        if (isset($_SESSION['id'])) {
-            echo '<a href="./Client/MonProfil.php" style="color: white; text-decoration: none; display: flex; align-items: center;">Mon Profil<img class="logo65" src="../../Assets/Logo/login.png"></a>';
-            echo '<a href="./Client/BackEnd/Deconnexion.php" style="color: white; text-decoration: none; display: flex; margin-left: 15px; align-items: center;">Déconnexion<img class="logo65" src="../../Assets/Logo/logout.jpg"></a>';
-        } else {
-            echo '<a href="./Client/Inscription.php" style="color: white; text-decoration: none; display: flex; align-items: center;">Inscription<img class="logo65" src="../../Assets/Logo/register.jpg"></a>';
-            echo '<a href="./Client/Connexion.php" style="color: white; text-decoration: none; display: flex; margin-left: 15px; align-items: center;">Connexion<img class="logo65" src="../../Assets/Logo/logout.jpg"></a>';
-        }
-        ?>
-    </div>
-</div>
+<?php
+include "Header.php";
+?>
 <body>
     <header>
         <h1>PersonaliTree</h1>
