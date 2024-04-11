e1222f7180f1c53e7bd9535fc173b47cecc380a7
A silent voice <liliandel.etoile@gmail.com>
2024-04-12 00:50:03 +0200

    Refactor Recommendation.php to optimize code and improve readability

--- PHP/Client/Recommendation.php
+++ PHP/Client/Recommendation.php
@@ -66,26 +66,36 @@ include "Header.php";
             
 
             <?php
+                $agemin=0;
+                $agemax=200;
+                foreach ($closest_users as $user) {
+                    $id = $user['id'];
 
+                    $query = "SELECT * FROM infopersos WHERE user_id = '$id'";
+                    $result = mysqli_query($con, $query);
+                    $prof = mysqli_fetch_assoc($result);
 
-                    foreach ($closest_users as $user) {
-                        $id = $user['id'];
-
-                        $query = "SELECT * FROM infopersos WHERE user_id = '$id'";
-                        $result = mysqli_query($con, $query);
-                        $prof = mysqli_fetch_assoc($result);
-
-                        echo('<div class="profil-trouvé">');
-                        echo('<img src="../../Assets/Client/ProfileImage/'.$prof["imgpath"].'"class="profil-img" width="200" length="200">');
-                        echo("<b><p>".$prof['prenom']." ".$prof['nom']."</p></b>");
-                        echo("<p><u> Genre:</u> ".$prof['sexe']."</p>");
-                        if($prof['dateNaissance']!=null){
-                            echo("<p><u> Age:</u> ".$age." ans</p>");
+                    if($prof['dateNaissance']!=null){
+                        $birthdate = new DateTime($prof['dateNaissance']);
+                        $today = new DateTime('today');
+                        $age = $birthdate->diff($today)->y;
+                        if($agemin>$age || $agemax<$age ){
+                            
+                            $verif=0;
                         }
-                        echo("<p>".$prof['ville']."</p>");
-                        echo("<p>".$prof['pays']."</p>");
-                        echo("</div>");
                     }
+
+                    echo('<div class="profil-trouvé">');
+                    echo('<img src="../../Assets/Client/ProfileImage/'.$prof["imgpath"].'"class="profil-img" width="200" length="200">');
+                    echo("<b><p>".$prof['prenom']." ".$prof['nom']."</p></b>");
+                    echo("<p><u> Genre:</u> ".$prof['sexe']."</p>");
+                    if($prof['dateNaissance']!=null){
+                        echo("<p><u> Age:</u> ".$age." ans</p>");
+                    }
+                    echo("<p>".$prof['ville']."</p>");
+                    echo("<p>".$prof['pays']."</p>");
+                    echo("</div>");
+                }
             ?>
         </table>
     </div>
