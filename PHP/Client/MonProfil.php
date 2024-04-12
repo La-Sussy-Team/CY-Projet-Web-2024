73cc4bff914128b0708b25a81160996eaf25b197
Sou <pierreantoine.cassard@gmail.com>
2024-04-12 10:01:30 +0200

    css profil

--- PHP/Client/MonProfil.php
+++ PHP/Client/MonProfil.php
@@ -103,7 +103,7 @@ include "Header.php";
                     <h2 style="text-align: center;"><?php echo($plante['plante']) ?></h2>
                 </div>
                 <div class="img-plant">
-                    <?php echo("<img src='../../Assets/Client/Plante/".$plante['imgpath']."'>"); ?>
+                    <?php echo("<img src='../../Assets/Client/Plante/".$plante['imgpath']." width='180' height='180' style='margin-left:20%;'>"); ?>
                 </div>
             </div>
         </div>
