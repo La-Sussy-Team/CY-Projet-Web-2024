81f2ff40b72dab5e380e6bcda0206660460c1ab3
Thomas <standage_thediet@8shield.net>
2024-04-12 23:53:54 +0200

    Commit Final

--- PHP/Client/BackEnd/LoginDatabase.php
+++ PHP/Client/BackEnd/LoginDatabase.php
@@ -1,7 +1,7 @@
 <?php
 $DATABASE_HOST = '127.0.0.1';
-$DATABASE_USER = 'testid';
-$DATABASE_PASS = 'testmdp';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = 'root';
 $DATABASE_NAME = 'personalytree';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 ?>
